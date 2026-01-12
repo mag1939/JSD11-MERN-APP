ec92f559c34d129f5c5749364f3f0f7481a0c91b
mag1939 <songsak.thawaro@gmail.com>
2026-01-12 09:36:07 +0700

    Setup Vitest

--- src/routes/v2/index.js
+++ src/routes/v2/index.js
@@ -1,8 +1,10 @@
 import { Router } from "express";
 import { router as usersRoutes } from "./users.routes.js"
+import { router as healthRoutes } from "./users.routes.js"
 
 // 2nd route!
 export const router = Router();
 
 // next route
 router.use("/users", usersRoutes);
+router.use("/health", healthRoutes);
